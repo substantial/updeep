30ff2553fcde9ddcc90be3863125794697c4ebe7
Aaron Jensen <aaronjensen@gmail.com>
2015-08-01 08:43:21 -0700

    Remove coverage stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,9 @@
 'use strict'; // eslint-disable-line
-var path = require('path');
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var excludeGitignore = require('gulp-exclude-gitignore');
 var mocha = require('gulp-mocha');
-var istanbul = require('gulp-istanbul');
 var nsp = require('gulp-nsp');
-var plumber = require('gulp-plumber');
-var coveralls = require('gulp-coveralls');
 var babel = require('gulp-babel');
 
 // Initialize the babel transpiler so ES2015 files gets compiled
@@ -26,37 +22,9 @@ gulp.task('nsp', function (cb) {
   nsp('package.json', cb);
 });
 
-gulp.task('pre-test', function () {
-  return gulp.src('lib/**/*.js')
-    .pipe(babel())
-    .pipe(istanbul({includeUntested: true}))
-    .pipe(istanbul.hookRequire());
-});
-
-gulp.task('test', ['pre-test'], function (cb) {
-  var mochaErr;
-
-  gulp.src('test/**/*.js')
-    .pipe(plumber())
-    .pipe(mocha({reporter: 'spec'}))
-    .on('error', function (err) {
-      console.log('err', err);
-      mochaErr = err;
-    })
-    .pipe(istanbul.writeReports())
-    .on('end', function () {
-      console.log('end', mochaErr);
-      cb(mochaErr);
-    });
-});
-
-gulp.task('coveralls', ['test'], function () {
-  if (!process.env.CI) {
-    return;
-  }
-
-  return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
-    .pipe(coveralls());
+gulp.task('test', function () {
+  return gulp.src('test/**/*.js')
+    .pipe(mocha({reporter: 'spec'}));
 });
 
 gulp.task('babel', function () {
@@ -66,4 +34,4 @@ gulp.task('babel', function () {
 });
 
 gulp.task('prepublish', ['nsp', 'babel']);
-gulp.task('default', ['static', 'test', 'coveralls']);
+gulp.task('default', ['static', 'test']);
