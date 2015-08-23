358406f12becb20701ad28ec28da3fd974fd4e95
Aaron Jensen <aaronjensen@gmail.com>
2015-08-23 10:48:32 -0700

    Add spec for reject by index

--- test/reject-spec.js
+++ test/reject-spec.js
@@ -2,6 +2,12 @@ import { expect } from 'chai';
 import u from '../lib';
 
 describe('u.reject', () => {
+  it('can reject by index', () => {
+    const result = u.reject((_, index) => index === 1, [3, 4, 5]);
+
+    expect(result).to.eql([3, 5]);
+  });
+
   it('freezes the result', () => {
     expect(Object.isFrozen(u.reject('a', []))).to.be.true;
   });
