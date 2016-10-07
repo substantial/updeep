cfe2ab9f60ec6eb4fcd5b50d8116133eb397b6cd
AveVlad <dev@vld.me>
2016-10-07 05:23:11 +0300

    Remove lodash/toString and fix tests in splitPath

--- test/util/splitPath-spec.js
+++ test/util/splitPath-spec.js
@@ -2,22 +2,19 @@ import { expect } from 'chai';
 import splitPath from '../../lib/util/splitPath';
 
 describe('splitPath', () => {
-  it('test number path', () => {
+  it('treats a number as a single step path', () => {
     const path = 1;
     const result = splitPath(path);
     expect(result).to.deep.equal(['1']);
   });
-  it('test number path', () => {
-    const path = 0;
-    const result = splitPath(path);
-    expect(result).to.deep.equal(['0']);
-  });
-  it('test array path', () => {
+
+  it('handles arrays', () => {
     const path = ['foo', 'bar', 'x'];
     const result = splitPath(path);
     expect(result).to.equal(path);
   });
-  it('test string path', () => {
+
+  it('handles strings separated by dots', () => {
     const path = 'bar.0.y';
     const result = splitPath(path);
     expect(result).to.deep.equal(['bar', '0', 'y']);
