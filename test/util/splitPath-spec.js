bc00fee109f51f89bdb9f8b8b4756e236f0b84b6
Vlad <AveVlad@users.noreply.github.com>
2016-10-07 06:14:03 +0300

    Allow Number type in updateIn function (#67)

---
+++ test/util/splitPath-spec.js
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+import splitPath from '../../lib/util/splitPath';
+
+describe('splitPath', () => {
+  it('treats a number as a single step path', () => {
+    const path = 1;
+    const result = splitPath(path);
+    expect(result).to.deep.equal(['1']);
+  });
+
+  it('handles arrays', () => {
+    const path = ['foo', 'bar', 'x'];
+    const result = splitPath(path);
+    expect(result).to.equal(path);
+  });
+
+  it('handles strings separated by dots', () => {
+    const path = 'bar.0.y';
+    const result = splitPath(path);
+    expect(result).to.deep.equal(['bar', '0', 'y']);
+  });
+});
